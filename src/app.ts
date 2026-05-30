2879ec44c540d9135f0cde57a4ee6ae70bc585be
husnearaa <hosnearapopy462@gmail.com>
2026-05-30 23:53:25 +0600

    created utility folder and installed cookie parser and cors

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,28 @@
+import CookieParser from "cookie-parser";
+import cors from "cors";
 import express, {
   type Application,
   type Request,
   type Response,
 } from "express";
+import logger from "./middleware/logger";
+import { authRoute } from "./modules/auth/auth.route";
 import { profileRoute } from "./modules/profile/profile.route";
 import { userRoute } from "./modules/user/user.route";
-import { authRoute } from "./modules/auth/auth.route";
-
+import globalErrorHandler from "./middleware/globalErrorHandler";
 const app: Application = express();
 
+app.use(CookieParser());
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
+app.use(logger);
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  }),
+);
 
 app.get("/", (req: Request, res: Response) => {
   //res.send("Hello World!");
@@ -24,4 +35,8 @@ app.get("/", (req: Request, res: Response) => {
 app.use("/api/users", userRoute);
 app.use("/api/profile", profileRoute);
 app.use("/api/auth", authRoute);
+
+// Global Error Handling Middleware
+app.use(globalErrorHandler);
+
 export default app;
